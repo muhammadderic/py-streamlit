bc9b418db866a7a91e328bca62979279f50132c7
muhammadderic <muhammad.donny.e@gmail.com>
2024-11-03 19:45:44 +0700

    Create a simple connection to supabase

--- app.py
+++ app.py
@@ -1,4 +1,15 @@
 import streamlit as st
+from st_supabase_connection import SupabaseConnection
 
 st.title("Hello Deric")
 st.write("This is a simple example of a Streamlit app.")
+
+# Initialize connection.
+conn = st.connection("supabase",type=SupabaseConnection)
+
+# Perform query.
+rows = conn.query("*", table="mytable", ttl="10m").execute()
+
+# Print results.
+for row in rows.data:
+    st.write(f"{row['name']} has a :{row['pet']}:")
